828c6450c397e09eb14b52528f65cc1fa535a863
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-04-10 19:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	if (sampleObject.key == undefined) {
+		return false
+	}
+	else
+	return true
 }
 
 // Do not change the code below
